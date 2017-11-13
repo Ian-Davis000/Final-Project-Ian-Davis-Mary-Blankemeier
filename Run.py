8b6e15c591272a55f9a346d2ab664957054ed792
Ian-Davis000 <33636194+Ian-Davis000@users.noreply.github.com>
2017-11-13 14:41:24 -0500

    Work

--- Run.py
+++ Run.py
@@ -5,33 +5,31 @@ import gamebox
 import pygame
 import math
 CAMERA_WIDTH, CAMERA_HEIGHT = 1000, 600
-BOX_WIDTH, BOX_HEIGHT = 20, 35
 camera = gamebox.Camera(CAMERA_WIDTH, CAMERA_HEIGHT)
 playeroneimage = 'Goku-1.png'
 playerone = gamebox.from_image(300, 400, playeroneimage)
 playerone.scale_by(1.75)
-background = gamebox.from_color(500, 600, "green", 700, 50)
+background = gamebox.from_color(500, 500, "green", 700, 50)
 ticks = 0
 music = gamebox.load_sound("https://upload.wikimedia.org/wikipedia/commons/3/3c/Beat_electronic.ogg")
 
 def tick(keys):
-    musicplay=msuic.play(-1)
+    global playeroneimage, playerone, yspeed, ticks
+    music.play(-1)
     ticks +=1
     scoredisplay = gamebox.from_text(0, 0, "SCORE: " + str(ticks // 30), "Arial", 14, "red", italic=True)
     scoredisplay.top = camera.top
     scoredisplay.right = camera.right
-    camera.draw(scoredisplay)
-    global playeroneimage, playerone, yspeed
-    camera.clear("black")
-    yspeed = playerone.yspeed
-    playerone = gamebox.from_image(playerone.x, playerone.y, playeroneimage)
-    playerone.yspeed = yspeed
+    camera.clear("blue")
 
-    if pygame.K_UP in keys:
+
+
+    if pygame.K_UP in keys and playerone.touches(background):
         playerone.yspeed = -10
 
     if pygame.K_DOWN in keys and playerone.touches(background):
         playeroneimage = 'Goku-crouch.png'
+        playerone.y = background.y -2
     elif pygame.K_DOWN not in keys and playerone.touches(background):
         playeroneimage = 'Goku-1.png'
     if pygame.K_RIGHT in keys:
@@ -40,11 +38,17 @@ def tick(keys):
     if pygame.K_LEFT in keys:
         playerone.x += -4
 
+    yspeed = playerone.yspeed
+    playerone = gamebox.from_image(playerone.x, playerone.y, playeroneimage)
+    playerone.yspeed = yspeed
+
     playerone.yspeed += .5
     playerone.y = playerone.y + playerone.yspeed
     if playerone.touches(background):
         playerone.move_to_stop_overlapping(background)
 
+
+    camera.draw(scoredisplay)
     camera.draw(background)
     camera.draw(playerone)
     camera.display()
